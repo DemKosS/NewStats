97568fd912d9cc3c2b9f2b80a4e25946948d7130
DemKosS <sidyakinkn@gmail.com>
2022-04-08 17:26:09 +0300

    Init

---
+++ src/test/java/ru/netology/StatsServiceTest.java
@@ -0,0 +1,72 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class StatsServiceTest {
+
+    @Test
+    void shouldCalcSum() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 180;
+
+        int actual = service.calcSum(sales);
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalcAverageSumInMonth() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 15;
+
+        int actual = service.calcAverageSumInMonth(sales);
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    void shouldMaxMonth() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+
+        int actual = service.maxSales(sales);
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    void shouldMinMonth() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+
+        int actual = service.minSales(sales);
+        Assertions.assertEquals(actual, expected);
+
+
+    }
+
+    @Test
+    void shouldAverageMin() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = service.averageMin(sales);
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    void shouldAverageMax() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = service.averageMax(sales);
+        Assertions.assertEquals(actual, expected);
+    }
+
+}
